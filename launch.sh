a7a3ea87cfdfcf138dccb391e038d42ad3a98401
Omer Cohen <omer@cohen.io>
2017-03-01 10:43:39 +0200

    add support for optional custom dns resolver
    in cases of dynamic envinronments where containers move around
    and respwan due to health checks, the IP of the forwarded host
    might change nand should be resolved in case it's not found
    this change introduces an optional dns resolver if it's needed
    more info at https://tenzer.dk/nginx-with-dynamic-upstreams/

--- launch.sh
+++ launch.sh
@@ -4,4 +4,8 @@ rm /etc/nginx/conf.d/default.conf || :
 envsubst < auth.conf > /etc/nginx/conf.d/auth.conf
 envsubst < auth.htpasswd > /etc/nginx/auth.htpasswd
 
+if [ -n "${DNS_RESOLVER}" ]; then
+  echo "resolver ${DNS_RESOLVER};" >> /etc/nginx/conf.d/auth.conf
+fi
+
 nginx -g "daemon off;"
